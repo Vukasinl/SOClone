1efaefba02171d6f3bdf9ddec7757258ddfe4a83
Vukasinl <lalosevic.vukasin@gmail.com>
2020-02-14 22:00:16 +0100

    Update best answer id on delete using Eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -18,6 +18,10 @@ class Answer extends Model
         return \Parsedown::instance()->text($this->body);
     }
 
+    public function getStatusAttribute(){
+        return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
+
     public function question(){
         return $this->belongsTo(Question::class);
     }
@@ -35,7 +39,12 @@ class Answer extends Model
         });
 
         static::deleted(function($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if($question->best_answer_id == $answer->id){
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
     }
 }
