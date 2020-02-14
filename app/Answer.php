8ae042a0398595d0f994ac1504362df3d21f16ca
Vukasinl <lalosevic.vukasin@gmail.com>
2020-02-14 23:29:12 +0100

    Save answer as best answer

--- app/Answer.php
+++ app/Answer.php
@@ -19,7 +19,15 @@ class Answer extends Model
     }
 
     public function getStatusAttribute(){
-        return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '';
+        return $this->isBest() ? 'vote-accepted' : '';
+    }
+
+    public function getIsBestAttribute(){
+        return $this->isBest();
+    }
+
+    private function isBest(){
+        return $this->id == $this->question->best_answer_id;
     }
 
     public function question(){
--- app/Question.php
+++ app/Question.php
@@ -38,6 +38,11 @@ class Question extends Model
         return \Parsedown::instance()->text($this->body);
     }
 
+    public function acceptBestAnswer(Answer $answer){
+        $this->best_answer_id = $answer->id;
+        $this->save();
+    }
+
     public function user(){
         return $this->belongsTo(User::class);
     }
