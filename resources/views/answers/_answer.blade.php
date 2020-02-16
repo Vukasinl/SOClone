f4c12828e52744f18070ee880ea6afdee71154f1
Vukasinl <lalosevic.vukasin@gmail.com>
2020-02-16 21:38:39 +0100

    Small refractors in views

---
+++ resources/views/answers/_answer.blade.php
@@ -0,0 +1,38 @@
+<div class="media post">
+
+    @include('shared._vote', [
+        'model' => $answer,
+    ])
+    <div class="media-body">
+        {!! $answer->body_html !!}
+        <div class="row">
+            <div class="col-4">
+                <div class="ml-auto">
+                    @auth
+                        @can('update', $answer)
+                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-outline-info btn-sm">Edit</a>
+                        @endcan
+
+                        @if(Auth::user()->can('delete', $answer))
+                            <form action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}" method="post" class="form-delete">
+                                @csrf
+                                @method('delete')
+
+                                <button type="submit" class="btn btn-outline-danger btn-sm" onclick="return confirm('Are you sure?')">Delete</button>
+                            </form>
+                        @endif
+                    @endauth
+                </div>
+            </div>
+            <div class="col-4">
+            </div>
+            <div class="col-4">
+                @include('shared._author', [
+                    'model' => $answer,
+                    'label' => 'answered',
+                ])
+            </div>
+        </div>
+
+    </div>
+</div>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -23,51 +23,7 @@ use Illuminate\Support\Str;
                         @include('layouts._messages')
 
                         @forelse($questions as $question)
-                            <div class="media">
-                                <div class="d-flex flex-column counters">
-                                    <div class="vote">
-                                        <strong>{{ $question->votes_count }}</strong> {{  \Illuminate\Support\Str::plural('vote', $question->votes_count) }}
-                                    </div>
-                                    <div class="status {{ $question->status }}">
-                                        <strong>{{ $question->answers_count }}</strong> {{  \Illuminate\Support\Str::plural('answer', $question->answers_count) }}
-                                    </div>
-                                    <div class="view">
-                                        {{ $question->views . " " . \Illuminate\Support\Str::plural('view', $question->views) }}
-                                    </div>
-                                </div>
-                                <div class="media-body">
-                                    <div class="d-flex align-items-center">
-                                        <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                        <div class="ml-auto">
-                                            @auth
-                                                @can('update', $question)
-                                                    <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-outline-info btn-sm">Edit</a>
-                                                @endcan
-
-                                                @if(Auth::user()->can('delete', $question))
-                                                    <form action="{{ route('questions.destroy', $question->id) }}" method="post" class="form-delete">
-                                                        @csrf
-                                                        @method('delete')
-
-                                                        <button type="submit" class="btn btn-outline-danger btn-sm" onclick="return confirm('Are you sure?')">Delete</button>
-                                                    </form>
-                                                @endif
-                                            @endauth
-                                        </div>
-                                    </div>
-
-                                    <p class="lead">
-                                        Asked by
-                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                        <small class="text-muted">{{ $question->created_date }}</small>
-                                    </p>
-                                    <div class="excerpt">
-                                        {{ $question->excerpt }}
-                                    </div>
-
-                                </div>
-                            </div>
-                            <hr>
+                            @include('questions._excerpt')
 
                         @empty
                             <div class="alert alert-warning">
